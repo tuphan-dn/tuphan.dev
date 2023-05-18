61571b90aa6cd8e7dcf4ae8d4e8b5ce4a69477e1
tuphan-dn <tuphan@descartes.network>
2023-05-18 19:03:26 +0700

    bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { useDebounce } from 'react-use'
 
 import Header from './header'
 
+import { env } from 'configs/env'
+
 export default function Home() {
   const [loading, setLoading] = useState(false)
 
@@ -18,14 +20,21 @@ export default function Home() {
   return (
     <Fragment>
       <Header />
-      <main className="flex min-h-[calc(100vh-64px-1rem)] space-x-4 ml-4 mb-4 p-4 rounded-l-xl bg-slate-100 dark:bg-slate-900">
-        <button
-          className={`btn-primary btn ${loading ? 'loading' : ''}`}
-          onClick={() => setLoading(true)}
-        >
-          Nothing
-        </button>
-        <input type="date" className="input" />
+      <main className="flex items-center min-h-[calc(100vh-64px-1rem)] space-x-4 ml-4 mb-4 p-4 rounded-l-xl bg-slate-100 dark:bg-slate-900">
+        <div>
+          <button
+            className={`btn-primary btn ${loading ? 'loading' : ''}`}
+            onClick={() => setLoading(true)}
+          >
+            Nothing
+          </button>
+        </div>
+        <div>
+          <input type="date" className="input" />
+        </div>
+        <div>
+          <p>{env}</p>
+        </div>
       </main>
     </Fragment>
   )
