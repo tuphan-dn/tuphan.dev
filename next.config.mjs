6d8c99059fdcd37b4f64335ea18cfe8a394133bd
tuphan-dn <tuphan@descartes.network>
2023-06-12 14:31:29 +0700

    debug

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ import remarkGfm from 'remark-gfm'
 import rehypeSlug from 'rehype-slug'
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
-const repo = (process.env.GITHUB_REPOSITORY || '').replace(/.*?\//, '')
-// const assetPrefix = isGithubActions ? `/${repo}/` : ''
-const basePath = isGithubActions ? `/${repo}` : ''
+const repo = isGithubActions
+  ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}`
+  : ''
 
 const withMDX = configMDX({
   extension: /\.mdx?$/,
@@ -17,8 +17,8 @@ const withMDX = configMDX({
 })
 
 const nextConfig = {
-  // assetPrefix,
-  basePath,
+  assetPrefix: repo,
+  basePath: repo,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   output: 'export',
   images: { unoptimized: true },
