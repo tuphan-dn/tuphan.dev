416bdcb451a94eb4576f24912d49660dfd26c5df
tuphan-dn <tuphan@descartes.network>
2023-06-15 09:46:01 +0700

    bugfix

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -10,12 +10,12 @@ export default function News() {
   return (
     <div className="grid grid-cols-12 gap-2">
       <h3 className="col-span-12">News</h3>
-      <div className="col-span-12">
-        <div className="grid grid-cols-12 gap-8 @container">
+      <div className="col-span-12 @container">
+        <div className="grid grid-cols-12 gap-1 @sm:gap-2 @xl:gap-3 @3xl:gap-4 @6xl:gap-5 ">
           {data.map((news, i) => (
             <div
               key={i}
-              className="col-span-12 @lg:col-span-6 @2xl:col-span-4 @4xl:col-span-3 @6xl:col-span-2"
+              className="col-span-12 @sm:col-span-6 @xl:col-span-4 @3xl:col-span-3 @6xl:col-span-2"
             >
               <NewsCard {...news} />
             </div>
