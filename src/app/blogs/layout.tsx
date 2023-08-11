ced4e0812b83c8e0b75861ae206035689e206bbe
tuphan-dn <tuphan@descartes.network>
2023-08-11 11:27:10 +0700

    exciting colors to read

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -10,8 +10,8 @@ export const metadata: Metadata = {
 
 export default function BlogsLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-full flex flex-row justify-center rounded-3xl bg-gradient-to-br from-purple-100 via-pink-100 to-amber-100 dark:from-purple-900 dark:via-pink-900 dark:to-amber-900 transition-all p-4">
-      <div className="w-full max-w-[1240px]">{children}</div>
+    <div className="w-full grid grid-cols-12 gap-2">
+      <div className="col-span-full">{children}</div>
     </div>
   )
 }
