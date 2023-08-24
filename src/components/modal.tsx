8c77e80a321551fbd65f99ead34e16f846b2ee81
tuphan-dn <tuphan@descartes.network>
2023-08-24 16:38:37 +0700

    fix modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Fragment, ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 import { X } from 'lucide-react'
 
@@ -14,21 +14,26 @@ export default function Modal({
   onCancel = () => {},
   children,
 }: ModalProps) {
+  const [ref, setRef] = useState<HTMLDialogElement | null>(null)
+
+  useEffect(() => {
+    if (!ref) return () => {}
+    if (open) ref.showModal()
+    else ref.close()
+  }, [open, ref])
+
   return (
-    <Fragment>
-      <input type="checkbox" className="modal-toggle" checked={open} readOnly />
-      <div className="modal">
-        <div className="modal-box max-h-fit">
-          <button
-            className="btn btn-circle btn-ghost btn-sm absolute top-2 right-2"
-            onClick={onCancel}
-          >
-            <X className="w-4 h-4" />
-          </button>
-          {children}
-        </div>
-        <label className="modal-backdrop" onClick={onCancel} />
-      </div>
-    </Fragment>
+    <dialog className="modal" ref={setRef}>
+      <form method="dialog" className="modal-box max-h-fit">
+        <button
+          className="btn btn-circle btn-ghost btn-sm absolute top-2 right-2"
+          onClick={onCancel}
+        >
+          <X className="w-4 h-4" />
+        </button>
+        {children}
+      </form>
+      <div className="modal-backdrop" onClick={onCancel} />
+    </dialog>
   )
 }
