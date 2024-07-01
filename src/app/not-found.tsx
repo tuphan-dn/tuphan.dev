f7b74c31c3365a79cd07d6b15e8fb3133226beb0
Tu Phan NE <tups@coin98.finance>
2024-07-01 17:12:19 +0700

    feat: env, configs

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,13 @@
 
 import Link from 'next/link'
 
+const host = (process.env.NEXT_PUBLIC_HOST || '').replace(/^https?:\/\//, '')
+
 export default function NotFound() {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center p-4 z-10">
-      <h1 className="text-primary">blogy.me/404 :(</h1>
-      <h3 className="mt-6">Not Found</h3>
+    <div className="w-full h-full flex flex-col items-center justify-center gap-0 p-4">
+      <h1 className="text-primary">{`${host}/404 :(`}</h1>
+      <h3 className="mt-8">Not Found</h3>
       <p className="opacity-60 text-center">
         Sorry, we could not find the requested resource.
       </p>
