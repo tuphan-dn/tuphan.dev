4009b6fcffffefc2050f32b69af6aca0f3053e8e
Tu Phan NE <tups@coin98.finance>
2024-07-01 15:08:15 +0700

    feat: eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: ['selector', '[data-theme="dark"]'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#0ea5e9', // sky-500
+          'primary-content': '#e0f2fe', // sky-100
+          secondary: '#0f172a', // slate-900
+          'secondary-content': '#f1f5f9', // slate-100
+        },
+        dark: {
+          ...require('daisyui/src/theming/themes')['dark'],
+          primary: '#0ea5e9', // sky-500
+          'primary-content': '#e0f2fe', // sky-100
+          secondary: '#f1f5f9', // slate-100
+          'secondary-content': '#0f172a', // slate-900
+        },
+      },
+    ],
+  },
+}
+
+export default config
