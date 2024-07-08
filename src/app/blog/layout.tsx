ed120a2d89025c1f6726287a751079d44a2eb601
Tu Phan NE <tups@coin98.finance>
2024-07-08 10:56:37 +0700

    feat: breadcrums

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,5 +1,7 @@
 import { type ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="w-full p-6 flex flex-col items-center">{children}</div>
+  return (
+    <div className="w-full p-6 flex flex-row justify-center">{children}</div>
+  )
 }
--- src/app/blog/template.tsx
+++ src/app/blog/template.tsx
@@ -1,9 +1,41 @@
-import { type ReactNode } from 'react'
+'use client'
+import { useMemo, type ReactNode } from 'react'
+import { useSelectedLayoutSegments } from 'next/navigation'
+
+import Link from 'next/link'
 
 export default function Template({ children }: { children: ReactNode }) {
+  const segments = useSelectedLayoutSegments()
+
+  const slugs = useMemo(
+    () => [
+      { name: 'blog', href: '/blog' },
+      ...segments.map((segment, i, segments) => ({
+        name: segment,
+        href: ['/blog', ...segments.slice(0, i), segment].join('/'),
+      })),
+    ],
+    [segments],
+  )
+
   return (
-    <article className="w-full my-32 prose prose-p:tracking-[-.25px]">
-      {children}
-    </article>
+    <div className="w-full flex flex-col gap-4 items-center">
+      <div className="-m-6 w-[calc(100%+3rem)] px-6 py-2 border-b-2 border-base-300">
+        <div className="breadcrumbs text-sm">
+          <ul>
+            {slugs.map(({ name, href }) => (
+              <li key={href}>
+                <Link className="opacity-60 capitalize" href={href}>
+                  {name.replace('-', ' ')}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <article className="w-full my-32 prose prose-p:tracking-[-.25px]">
+        {children}
+      </article>
+    </div>
   )
 }
