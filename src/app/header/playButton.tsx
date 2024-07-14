9271442719d3e477bebacfe5c02e5faf1204de13
tuphan-dn <tuphan@descartes.network>
2024-07-14 14:52:01 +0700

    fix: rerender on template

--- src/app/header/playButton.tsx
+++ src/app/header/playButton.tsx
@@ -11,31 +11,32 @@ export default function PlayButton() {
   const [playing, setPlaying] = useState(false)
 
   return (
-    <Island>
-      <ReactPlayer
-        url="https://soundcloud.com/sontuph/city-pop-rain-lofi-ghibli-inspired-atmosphere-for-study-chillout-focus-relax"
-        playing={playing}
-        className="hidden"
-      />
-      <button
-        className="btn btn-sm rounded-full"
-        onClick={() => setPlaying(!playing)}
+    <button
+      className="btn btn-sm rounded-full"
+      onClick={() => setPlaying(!playing)}
+    >
+      <Island>
+        <ReactPlayer
+          className="hidden"
+          url="https://soundcloud.com/sontuph/city-pop-rain-lofi-ghibli-inspired-atmosphere-for-study-chillout-focus-relax"
+          playing={playing}
+          loop
+        />
+      </Island>
+      chillout
+      <motion.div
+        className={clsx(
+          'w-4 h-4 rounded-full bg-base-content flex flex-col items-center justify-center animate-[spin_2s_linear_infinite]',
+          {
+            '[animation-play-state:running]': playing,
+            '[animation-play-state:paused]': !playing,
+          },
+        )}
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
       >
-        chillout
-        <motion.div
-          className={clsx(
-            'w-4 h-4 rounded-full bg-base-content flex flex-col items-center justify-center animate-[spin_2s_linear_infinite]',
-            {
-              '[animation-play-state:running]': playing,
-              '[animation-play-state:paused]': !playing,
-            },
-          )}
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-        >
-          <Music className="w-2 h-2 text-base-100" />
-        </motion.div>
-      </button>
-    </Island>
+        <Music className="w-2 h-2 text-base-100" />
+      </motion.div>
+    </button>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next'
 
 import UiProvider from '@/providers/ui.provider'
 
+import Header from './header'
+import Footer from './footer'
+
 export const metadata: Metadata = {
   title: 'tuphan.dev',
   description:
@@ -34,7 +37,19 @@ export default function Layout({ children }: { children: ReactNode }) {
         />
       </head>
       <body>
-        <UiProvider>{children}</UiProvider>
+        <UiProvider>
+          <main className="w-full min-h-dvh bg-base-100 flex flex-col gap-0 justify-center">
+            <header className="w-full sticky top-0 z-10">
+              <Header />
+            </header>
+            <section className="w-full grow grid grid-cols-12 gap-0">
+              <div className="col-span-full">{children}</div>
+            </section>
+            <footer className="w-full">
+              <Footer />
+            </footer>
+          </main>
+        </UiProvider>
       </body>
     </html>
   )
