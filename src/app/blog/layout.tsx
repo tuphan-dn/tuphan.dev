e8a14fba5d13e3bcb328466b8a9900450eee6c95
tuphan-dn <tuphan@descartes.network>
2024-07-16 11:06:57 +0700

    feat: seo headers

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,4 +1,18 @@
 import { type ReactNode } from 'react'
+import type { Metadata } from 'next'
+import { headers } from 'next/headers'
+import { table } from '@/db'
+
+export async function generateMetadata(): Promise<Metadata> {
+  const href = headers().get('referer') || ''
+  const { pathname } = new URL(href)
+  const { title, description } =
+    table.find(({ route }) => route === pathname) || {}
+  return {
+    title,
+    description,
+  }
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
