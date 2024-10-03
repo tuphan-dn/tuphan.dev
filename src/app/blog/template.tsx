333df399f32eea3abe645e73b114881cb7eece20
tuphan-dn <tuphan@descartes.network>
2024-10-03 23:29:38 +0700

    fix: avoid scr

--- src/app/blog/template.tsx
+++ src/app/blog/template.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from 'react'
+import { use, type ReactNode } from 'react'
 import { headers } from 'next/headers'
 import ky from 'ky'
 
@@ -10,16 +10,14 @@ import Contributors from '@/components/contributors'
 import Schedule from '@/components/schedule'
 import Header from './header'
 
-export default async function Template({ children }: { children: ReactNode }) {
+export default function Template({ children }: { children: ReactNode }) {
   const pathname = headers().get('x-forwarded-pathname') || ''
   const {
     authors = [],
     tags = [],
     children: routes = [],
     date,
-  } = await ky
-    .get(`${process.env.NEXT_PUBLIC_HOST}/api${pathname}`)
-    .json<Blog>()
+  } = use(ky.get(`${process.env.NEXT_PUBLIC_HOST}/api${pathname}`).json<Blog>())
 
   return (
     <div className="w-full flex flex-col gap-4 items-center relative">
