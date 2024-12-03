5d121ad3fa8521438a1b131727bc6571f973abe6
tuphan-dn <tuphan@descartes.network>
2024-12-03 14:45:47 +0700

    chore: ngay thang he

---
+++ src/app/blog/ngay-nang-he/page.md
@@ -0,0 +1,14 @@
++++
+tags = "tản văn, 🇻🇳"
+date = "3 December, 2024"
++++
+
+# Ngày nắng hè
+
+Trích một đoạn trong "Khói trời lộng lẫy" của chị Tư ([Nguyễn Ngọc Tư](https://www.facebook.com/nguyenngoc4)). Dù chị Tư sinh năm 1976, nghĩa là lớn hơn tôi tận 18 tuổi, nhưng gọi chị là chị vì văn chị trẻ dữ lắm. Chị Tư viết sao giản dị mà lại thiệt tình và hay quá.
+
+_Tôi thấy mình nghèo quá, mình chỉ có tình thương. Tôi chỉ có thể hứa những lời hứa nghèo, chừng nào hái bí mẹ mua cho con bộ đồ mới, chừng nào con lớn mẹ sẽ cho đi chơi thành phố, chừng nào kiếm nhiều tiền mẹ mua chiếc xe đạp cho con chạy lòng vòng cồn chơi. Hoặc lời hứa mịt mù kiểu như "nghe lời mẹ rồi mẹ thương…"._
+
+![Nhà cũ.](./IMG_0708.JPG 'panorama')
+
+Đọc đoạn này tôi cứ miên man nhớ về ngày tháng bé xíu. Hình ảnh xóm Bầu hiện lên trong tôi vẫn luôn là những ngày nắng, gió lộng. Cũng có lẽ vì thế mà tôi lấy tựa "Ngày nắng hè" là thích hợp nhất cho những đoạn bình dị này.
--- src/components/md/image.tsx
+++ src/components/md/image.tsx
@@ -2,33 +2,71 @@
 import { type ComponentProps, useMemo } from 'react'
 import clsx from 'clsx'
 
-import Zoom from 'react-medium-image-zoom'
+import View360, { ControlBar, CylindricalProjection } from '@egjs/react-view360'
+import MediumImageZoom from 'react-medium-image-zoom'
+import Island from '@/components/island'
 
+import '@egjs/react-view360/css/view360.min.css'
 import 'react-medium-image-zoom/dist/styles.css'
 
-export type ImageProps = ComponentProps<'img'>
+type InternalProps = {
+  src: string
+  className?: string
+  alt?: string
+}
 
-export default function Image({
-  alt = '',
-  src: meta = '',
-  className,
-  ...props
-}: ImageProps) {
-  const { src } = useMemo(() => {
-    if (typeof meta === 'string') return { src: meta }
-    return meta
-  }, [meta])
+function Panorama({ src, className = '', alt = '' }: InternalProps) {
+  const projection = useMemo(
+    () => new CylindricalProjection({ src, partial: true }),
+    [src],
+  )
+  const plugins = useMemo(() => [new ControlBar()], [])
   return (
     <>
-      <Zoom wrapElement="span">
+      <View360
+        tag="span"
+        className={clsx('block h-[50dvh] mb-2 rounded-box', className)}
+        canvasClass="outline-none"
+        projection={projection}
+        plugins={plugins}
+        initialZoom={0}
+      />
+      <span className="text-xs">{alt}</span>
+    </>
+  )
+}
+
+function Zoom({ src, className = '', alt = '' }: InternalProps) {
+  return (
+    <>
+      <MediumImageZoom wrapElement="span">
         <img
           className={clsx('mb-2 rounded-box', className)}
           src={src}
           alt={alt}
-          {...props}
         />
-      </Zoom>
+      </MediumImageZoom>
       <span className="text-xs">{alt}</span>
     </>
   )
+}
+
+export default function Image({
+  alt = '',
+  title: meta = '',
+  src: url = '',
+  className,
+}: ComponentProps<'img'>) {
+  const { src } = useMemo(
+    () => (typeof url === 'string' ? { src: url } : url),
+    [url],
+  )
+
+  if (meta === 'panorama')
+    return (
+      <Island>
+        <Panorama className={className} src={src} alt={alt} />
+      </Island>
+    )
+  return <Zoom className={className} src={src} alt={alt} />
 }
